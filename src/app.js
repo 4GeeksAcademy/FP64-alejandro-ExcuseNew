25788779e5f98a4c4c8eabbd689cd2b2e8ccffcc
alejo0022 <156907082+alejo0022@users.noreply.github.com>
2024-03-27 10:57:51 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,57 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector("#btn").addEventListener("click", () => {
+    document.querySelector("#excuse").innerHTML = message();
+  });
+  const getRamdonElement = array => {
+    const ramdonIndex = Math.floor(Math.random() * array.length);
+    return array[ramdonIndex];
+  };
+
+  const getGuilty = () => {
+    const who = ["The dog", "My grandma", "The mailman", "My bird"];
+    return getRamdonElement(who);
+  };
+
+  const getAction = () => {
+    const action = ["ate", "peed", "crushed", "broke"];
+    return getRamdonElement(action);
+  };
+
+  const getTarget = () => {
+    const target = ["my homework", "my phone", "the car"];
+    return getRamdonElement(target);
+  };
+
+  const getWhen = () => {
+    const when = [
+      "before the class",
+      "when I was sleeping",
+      "while I was exercising",
+      "during my lunch",
+      "while I was praying"
+    ];
+    return getRamdonElement(when);
+  };
+
+  const getMessage = (guilty, action, target, when) => {
+    return `${guilty} ${action} ${target} ${when}`;
+  };
+
+  const printMessage = message => {
+    console.log(message);
+  };
+
+  function main() {
+    const guilty = getGuilty();
+    const action = getAction();
+    const target = getTarget();
+    const when = getWhen();
+
+    const message = getMessage(guilty, action, target, when);
+    printMessage(message);
+  }
+
+  main();
 };
